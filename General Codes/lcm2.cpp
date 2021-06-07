a7e27b76496ea6acd64d9c875f8b96c7025e8a10
DigvijaySawale <67545979+DigvijaySawale@users.noreply.github.com>
2021-06-07 21:17:11 +0530

    lcm of numbers

---
+++ General Codes/lcm2.cpp
@@ -0,0 +1,27 @@
+// optimized solution for lcm of two numbers
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if(b ==0)
+		return a;
+		
+	return gcd(b, a%b);
+}
+
+int lcm(int a , int b)
+{
+	return (a*b)/gcd(a,b);
+}
+
+int main()
+{
+	cout << lcm(18, 12) << endl;
+}
+
+/*
+    using formula 
+    
+    a*b = gcd(a,b) * lcm(a ,b )
