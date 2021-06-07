b330e6d43c77bd03c1543f42163c5ac32d94ee96
DigvijaySawale <67545979+DigvijaySawale@users.noreply.github.com>
2021-06-07 21:18:07 +0530

    power set of number

---
+++ General Codes/printPowerSet.cpp
@@ -0,0 +1,33 @@
+//power set generation using bitwise 
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void printPowerSet(string str)
+{
+	int n = str.length();
+
+	int powSize = pow(2, n);
+
+	for(int counter = 0; counter < powSize; counter++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if((counter & (1 << j)) != 0)
+                cout<<str[j];
+		}
+		
+		cout<<endl;
+	}
+}
+    
+
+    
+int main() {
+	
+	string s = "abc";
+
+    printPowerSet(s);  
+    
+}
