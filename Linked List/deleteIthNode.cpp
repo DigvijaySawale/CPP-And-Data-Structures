7389f72797b1263202ce1b1124604b7e49432bc6
DigvijaySawale <67545979+DigvijaySawale@users.noreply.github.com>
2021-06-19 16:42:09 +0530

    Delete the given node of linked list

---
+++ Linked List/deleteIthNode.cpp
@@ -0,0 +1,97 @@
+//delete ith node of linked list
+//insert node at ith position in linked list
+
+#include<iostream>
+using namespace std;
+#include "NodeClass.cpp"
+
+//function to delete node from ith position
+Node * deleteNode(Node *head, int i)
+{
+	Node *temp = head;        //creating temp variable
+	int count = 0;
+	
+	if(i == 0)            //to delete first node of ll
+	{
+		temp = temp -> next;
+		delete head;               //deallocating memory
+		head = temp;
+		return head;
+	}
+	
+	while(temp != NULL && count <i -1)      //to avoid traversing greater than the length of linked list
+	{
+		temp = temp -> next;    //pointing to i - 1 node
+		count++;
+	}
+	
+	if(temp != NULL)
+	{
+		Node *a = temp -> next;      //creating new nod to delete it
+		temp -> next = a -> next;     //linking the current node to next node of deleted one
+		delete a;                   //deallocation of unused memory
+	}
+	
+	return head;	
+}
+
+
+//using the previous functions to create user linked list and print it
+Node* takeInput_efficient()           //return type is node
+{
+    int data;
+    //cout << "->";
+    cin >> data;
+    Node *head = NULL;      //creating head pointer to store address of first node
+    Node *tail = NULL;      //tail pointer to store address of last node
+
+    while(data != -1)          //setting condition
+    {
+        Node * newNode = new Node(data);     //creating node dynamically to avoid automatic memory deallocation
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;        //making tail to point on first node
+        }
+        else
+        {
+        	tail -> next = newNode;     //creating link between current last node and new node
+        	tail = tail -> next;     // moving tail to new node which is now last node
+        	//OR
+        	//tail = newNode;
+        }
+        
+        //cout << "->";
+        cin >> data;
+    }
+
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+
+}
+
+int main()
+{
+	Node *head = takeInput_efficient();
+	cout << "Current linked list" << endl;
+	print(head);
+	cout << endl;
+	
+	int i;
+	cout <<"Enter position to delete-" << endl;
+	cin >> i;
+	
+	
+	head = 
+	deleteNode(head, i);
+	print(head);
+}
