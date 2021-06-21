1ce1ba487f8f480ac2b67290f0d51b883dcf7b12
DigvijaySawale <67545979+DigvijaySawale@users.noreply.github.com>
2021-06-21 23:07:27 +0530

    Reverse elements of linked list using recursion

---
+++ Linked List/reverseLLUsingRecursion.cpp
@@ -0,0 +1,44 @@
+//reversing linked list using recursion
+
+#include<iostream>
+using namespace std;
+
+#include "llStructure.cpp"
+//refer llStructure.cpp for class and createLL and print functions
+
+
+Node * reverseLL(Node *head)
+{
+	//base case
+	if(head == NULL || head -> next == NULL)
+	{
+		return head;
+	}
+	
+	//recursion call
+	Node * smallAns = reverseLL(head -> next);
+	
+	//small calculations
+	Node *temp = smallAns;
+	while(temp -> next != NULL)
+	{
+		temp = temp -> next;
+	}
+	
+	temp -> next = head;
+	head -> next = NULL;
+	return smallAns;
+}
+
+
+int main()
+{
+	cout << "Enter the data elements of linked list: ";
+	Node *head = createLL();
+	print(head);
+	cout << endl;
+	
+	Node *reversed = reverseLL(head);
+	cout << "The reversed linked list is: " ;
+	print(reversed); 
+}
